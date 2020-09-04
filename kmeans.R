8695bd14b7018d592145ca22b7855df25391c93e
Aydin Ahmadli <35424859+buzzer4mornin@users.noreply.github.com>
2020-09-04 17:17:53 +0200

    raw_data_df preparation

--- kmeans.R
+++ kmeans.R
@@ -7,3 +7,25 @@ library(mice)
 library(VIM)
 
 load("~/Desktop/R_hands_on/Search engine/sales_ah_1Y_[all_states].Rda")
+
+#------------------------------------------ Prepare "raw_data_df"  ----------------------------------------------
+raw_data_df =
+  sales_ah %>%
+  select(PRODUCT,
+         SHIPTO_CUSTOMER_ID,
+         SHIPTO_CUSTOMER_NAME,
+         QUOTA_GROUPING,
+         PRODUCT_GROUPING,
+         GROSS_SALES,
+         TIER,
+         PRODUCT_SPECIES,
+         REGION_CODE,
+         STATE,
+         GL_PERIOD,
+         INVOICE_DATE,
+         SALES_REP,
+         PRIMARY_COT,
+         CORP_ACCOUNT_NM,
+         GL_MONTH,
+         GL_QRT,
+         ZIP) %>%
