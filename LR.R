5cf0370e5623ba60a77ebab5d31e96e38b5af60b
Aydin Ahmadli <35424859+buzzer4mornin@users.noreply.github.com>
2020-09-11 18:54:18 +0200

    Prepare Raw Data

--- LR.R
+++ LR.R
@@ -12,3 +12,33 @@
   library(sinkr)
 
 load("~/Desktop/R_hands_on/Search engine/sales_ah_1Y_[all_states].Rda")
+
+#------------------------------------------  Prepare "raw_data_df"  ---------------------------------------------
+raw_data_df =
+  sales_ah %>%
+  select(PRODUCT,
+         SHIPTO_CUSTOMER_ID,
+         SHIPTO_CUSTOMER_NAME,
+         QUOTA_GROUPING,
+         PRODUCT_GROUPING,
+         GROSS_SALES,
+         TIER,
+         PRODUCT_SPECIES,
+         REGION_CODE,
+         STATE,
+         GL_PERIOD,
+         INVOICE_DATE,
+         SALES_REP,
+         PRIMARY_COT,
+         CORP_ACCOUNT_NM,
+         GL_MONTH,
+         GL_QRT,
+         ZIP) %>%
+  filter(PRODUCT_GROUPING != 0) %>%
+  mutate(QUOTA_GROUPING = PRODUCT_GROUPING) %>%
+  mutate(QUOTA_GROUPING = gsub(pattern = " ", replacement = "_", QUOTA_GROUPING),
+         QUOTA_GROUPING = gsub(pattern = "-", replacement = "_", QUOTA_GROUPING),
+         QUOTA_GROUPING = gsub(pattern = "_&_", replacement = "_", QUOTA_GROUPING)) %>%
+  mutate(SHIPTO_CUSTOMER_NAME = gsub("THE", "", SHIPTO_CUSTOMER_NAME)) %>%
+  mutate(SHIPTO_CUSTOMER_NAME = gsub("  ", " ", SHIPTO_CUSTOMER_NAME)) %>%
+  mutate(SHIPTO_CUSTOMER_NAME = trimws(SHIPTO_CUSTOMER_NAME))
