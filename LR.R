05283a9194240f18a1b8944d2d85e26f580a3bc5
Aydin Ahmadli <35424859+buzzer4mornin@users.noreply.github.com>
2020-09-27 22:13:26 +0200

    computations regarding WelchTest

--- LR.R
+++ LR.R
@@ -173,3 +173,13 @@ print(paste(val,"--",lTest[1,3]))
 
 # GL_MONTH - from factor to numeric
 table1_month$GL_MONTH <- as.numeric(table1_month$GL_MONTH)
+
+for (val in product_names)
+{
+  df = 
+    table1_month %>%
+    filter(QUOTA_GROUPING == val) 
+  
+  welch_test <- welch.test(GROSS_SALES ~ GL_MONTH, df,na.rm = TRUE,verbose=FALSE)
+  print(paste(val,"--",welch_test[3]))
+}
